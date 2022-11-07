5ede23c5a0d6968c27a902fdb16f27b74af15dad
Carolina Alisio <calisio@wustl.edu>
2022-11-06 23:56:18 -0600

    users correctly displayed on pages, chat function working in all rooms

--- chatroom.js
+++ chatroom.js
@@ -51,18 +51,30 @@ io.sockets.on("connection", function (socket) {
         }
         return userNicknameArray;
     }
+    
+    function updateHomeroomUserArray(){
+        let homeroomUserArray = chatRoomList["homeroom"];
+        let index = 0;
+        for (let i = 0; i < homeroomUserArray.length; i++){
+            if (homeroomUserArray[i][0] == user.id){
+                index = i;
+            }
+        }
+        homeroomUserArray.splice(index, 1);
+        return homeroomUserArray;
+    }
 
     socket.on('userLoggedOn', function (data) {
         user["nickname"] = data["nickname"];
         chatRoomList["homeroom"].push([user.id, user.nickname]);
-        console.log(chatRoomList["homeroom"]);
         socket.join("homeroom");
         let userNicknameArray = createNicknameArray();
-        console.log(userNicknameArray);
+        //userNicknameArray checked, returns the right thing
         io.sockets.in("homeroom").emit("sendingChatRoomList", { users: userNicknameArray, chatRoomList: Object.keys(chatRoomList) })
     });
 
     socket.on('createNewChatRoom', function (data) {
+        //creates new chat room and adds the user
         chatRoomList[data["chatRoomName"]] = [[user.id, user.nickname]];
         socket.leave("homeroom");
 
@@ -75,21 +87,14 @@ io.sockets.on("connection", function (socket) {
         let adminRoomName = user.room + "ADMIN";
         socket.join(adminRoomName);
 
-        let homeroomUserArray = chatRoomList["homeroom"];
-        let index = 0;
-        for (let i = 0; i < homeroomUserArray.length; i++){
-            if (homeroomUserArray[i][0] == user.id){
-                index = i;
-            }
-        }
-        homeroomUserArray.splice(index, 1);
-        //console.log(chatRoomList["homeroom"])
-        io.sockets.in("homeroom").emit("userLeftHomeroom", {user: homeroomUserArray});
+        //update the homeroom users array in chatRoomList
+        let homeroomUserArray = updateHomeroomUserArray(); // correctly removes the user from chatRoomList["homeroom"]
 
+        io.sockets.in("homeroom").emit("broadcastingUserLeftRoom", {users: homeroomUserArray, nickname: user.nickname});
+        
         let userNicknameArray = createNicknameArray();
-        io.sockets.in("homeroom").emit("updateRoomList", { chatRoomList: Object.keys(chatRoomList) });
+        io.sockets.in("homeroom").emit("updateRoomList", { users: homeroomUserArray, chatRoomList: Object.keys(chatRoomList) });
         io.sockets.in(user.room).emit("chatRoomCreated", { users: userNicknameArray, chatRoomName: user.room, nickname: user.nickname });
-        //console.log(socket.rooms);
     });
 
     socket.on('joinRoom', function (data) {
@@ -101,19 +106,11 @@ io.sockets.on("connection", function (socket) {
         chatRoomList[user.room].push([user.id, user.nickname]);
         socket.join(user.room);
 
-        let homeroomUserArray = chatRoomList["homeroom"];
-        let index = 0;
-        for (let i = 0; i < homeroomUserArray.length; i++){
-            if (homeroomUserArray[i][0] == user.id){
-                index = i;
-            }
-        }
-        homeroomUserArray.splice(index, 1);
-        console.log(chatRoomList["homeroom"])
+        let homeroomUserArray = updateHomeroomUserArray();
 
         let userNicknameArray = chatRoomList[user.room];
 
-        io.sockets.in("homeroom").emit("userLeftHomeroom", {user: homeroomUserArray});
+        io.sockets.in("homeroom").emit("broadcastingUserLeftRoom", {users: homeroomUserArray, nickname: user.nickname});
         io.sockets.in(user.room).emit("userJoinedRoom", { users: userNicknameArray, nickname: user.nickname, chatRoomName: user.room }) ;
         let adminRoomName = user.room + "ADMIN";
         io.sockets.in(adminRoomName).emit("userJoinedRoomADMIN", {users: userNicknameArray});
@@ -152,8 +149,8 @@ io.sockets.on("connection", function (socket) {
         let userNicknameArray = createNicknameArray();
         io.sockets.in(user.room).emit("broadcastingUserLeftRoom", { users: userNicknameArray, nickname: user.nickname });
         chatRoomList["homeroom"].push([user.id, user.nickname]);
+        user.room = "homeroom";
         socket.join("homeroom");
-        console.log(chatRoomList["homeroom"]);
         io.sockets.in("homeroom").emit("updateRoomList", {users: chatRoomList["homeroom"], chatRoomList: Object.keys(chatRoomList) });
         socket.emit("rejoinHomeroom", {nickname : user.nickname});
     });
@@ -234,6 +231,12 @@ io.sockets.on("connection", function (socket) {
 
         //TODO: somewhere else, check if room is in banned list before join
 
+    });
+
+    //----------------------------------sending messages-----------------------------------------------
+
+    socket.on("sendMessage", function(data){
+        io.sockets.in(user.room).emit("displayMessage", {nickname: user.nickname, message: data["message"]});
     });
 
 
